d76f73ead5aabc4dbe5c8a1261ebdbbb86d0a8d0
unknown <Admin1@CL-NB-609.tsoftglobal.com>
2020-11-25 17:37:37 -0300

    Primer Commit

---
+++ src/test/java/Utils.java
@@ -0,0 +1,4 @@
+public class Utils {
+    final static String BASE_URL = "https://formy-project.herokuapp.com/form";
+    final static String CHROME_DRIVER_LOCATION = "C:\\Users\\Admin1\\Downloads\\PruebaBasica\\chromedriver.exe";
+}
