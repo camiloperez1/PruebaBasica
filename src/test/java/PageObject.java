f83e2b721170ebb25516d162355c27c9f5a52541
unknown <Admin1@CL-NB-609.tsoftglobal.com>
2020-11-27 18:11:27 -0300

    PruebaGit

--- src/test/java/PageObject.java
+++ src/test/java/PageObject.java
@@ -1,4 +1,5 @@
 // All Test Pages are inheriting from this class
+//cosas que pueden seguir para cualquier pagina
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
